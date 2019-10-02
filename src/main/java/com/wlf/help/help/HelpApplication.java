cb572fc32b868d7961863c187a1f391899d073b7
wanglong321 <845627706@qq.com>
2019-10-02 15:17:23 +0800

    init repo

---
+++ src/main/java/com/wlf/help/help/HelpApplication.java
@@ -0,0 +1,13 @@
+package com.wlf.help.help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpApplication.class, args);
+	}
+
+}
